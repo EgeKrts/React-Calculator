1e0b59e3fd0b4225ccb026eb05c2636dfafb7f50
EgeKaratas <segekaratas@gmail.com>
2022-07-05 10:01:53 +0300

    Buttons are created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+
+        <div className="operators">
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+
+          <button>DEL</button>
+        </div>
+
+        <div className="numbers">
+          <button>9</button>
+          <button>8</button>
+          <button>7</button>
+          <button>6</button>
+          <button>5</button>
+          <button>4</button>
+          <button>3</button>
+          <button>2</button>
+          <button>1</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
